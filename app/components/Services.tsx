1f96ac5a8a897b11b1ad21b0dc20cae65dec2468
B2005848 <namltgt2002@gmail.com>
2025-08-16 21:58:43 +0700

    update

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -6,7 +6,7 @@ import "../styles/Services.css";
 export default function Services() {
   return (
     <section id="services" className="mb-5 bg-white ">
-      <div className="flex justify-end mb-5 display ">
+      <div className="flex justify-end mb-5 display autoShow ">
         <Image
           src="/contreo.svg"
           alt="Dịch vụ xe container bốc xếp hàng hóa"
@@ -17,10 +17,10 @@ export default function Services() {
       </div>
 
       <div>
-        <h3 className="mb-10 text-3xl font-bold text-center">
+        <h3 className="mb-10 text-3xl font-bold text-center ">
           ⚡Dịch vụ của chúng tôi⚡
         </h3>
-        <ul className="grid max-w-4xl grid-cols-1 gap-6 mx-auto md:grid-cols-2">
+        <ul className="grid max-w-4xl grid-cols-1 gap-6 mx-auto md:grid-cols-2 ">
           <li className="p-6 rounded-lg shadow-md paragraph_service bg-gray-50">
             <p>Bốc xếp hàng hóa đóng container</p>
           </li>
